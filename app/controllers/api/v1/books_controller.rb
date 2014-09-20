bc0f3ed43d7042c6ee0e7b36243d99d70633058c
Matthew Du <du.matthew@gmail.com>
2014-09-20 12:39:07 -0400

    add create action

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,6 +7,13 @@ module Api
 			end
 
 			def create
+				binding.pry
+				book = Book.new(book_params)
+				if book.save
+					render json: book
+				else
+					render json: {message: 'Error'}, status: 400
+				end
 			end
 
 			def show
@@ -16,6 +23,11 @@ module Api
 			end
 
 			def destroy
+			end
+
+			private
+			def book_params
+				params.require(:book).permit(:course_code, :title, :author, :publisher, :published_date, :seller_name, :seller_email, :seller_phone_number)
 			end
 
 		end
