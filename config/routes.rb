7b31355e22d8e16f0a0a3d641a81f7f19807422b
Matthew Du <du.matthew@gmail.com>
2014-09-20 12:40:59 -0400

    add subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: 'json'},
+                  constraints: {subdomain: 'api'}, path: '/' do
     namespace :v1 do
       resources :books
     end
   end
 end
-
