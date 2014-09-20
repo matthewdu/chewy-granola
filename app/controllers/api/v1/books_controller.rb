33de52d631619ac54aaeed14bf10ed6e45522ea1
Matthew Du <du.matthew@gmail.com>
2014-09-20 14:35:35 -0400

    finish books_controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,11 +3,14 @@ module Api
 		class BooksController < ApplicationController
 
 			def index
-				render json: {message: 'Resource not found'}, status: 404
+				if params[:course_code]
+					book = Book.where(course_code: params[:course_code])
+				end
+				# book = Book.all
+				render json: book
 			end
 
 			def create
-				binding.pry
 				book = Book.new(book_params)
 				if book.save
 					render json: book
@@ -24,14 +27,26 @@ module Api
 			end
 
 			def update
+				book = Book.find(params[:id])
+				if book.update_attributes(book_params)
+					render json: book
+				else
+					render json: {message: 'Error'}
+				end
+			rescue ActiveRecord::RecordNotFound
+				render json: {message: 'RecordNotFound'}
 			end
 
 			def destroy
+				book = Book.find(params[:id]).destroy
+				render json: book
+				rescue ActiveRecord::RecordNotFound
+				render json: {message: 'RecordNotFound'}
 			end
 
 			private
 			def book_params
-				params.require(:book).permit(:course_code, :title, :author, :publisher, :published_date, :seller_name, :seller_email, :seller_phone_number)
+				params.require(:book).permit(:course_code, :title, :author, :publisher, :publication_date, :seller_name, :seller_email, :seller_phone_number)
 			end
 
 		end
