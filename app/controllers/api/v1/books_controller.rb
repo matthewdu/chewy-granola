9b65117dbcbc568dedb531aa72c7113df659d682
Matthew Du <du.matthew@gmail.com>
2014-09-20 20:20:50 -0400

    remove cors

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,6 @@
 module Api
 	module V1
 		class BooksController < ApplicationController
-			before_filter :set_headers
 
 			def index
 				if params[:course_code]
@@ -49,14 +48,6 @@ module Api
 			def book_params
 				params.require(:book).permit(:course_code, :title, :author, :publisher, :publication_date, :seller_name, :seller_email, :seller_phone_number)
 			end
-
-		  def set_headers
-    		headers['Access-Control-Allow-Origin'] = '*'
-    		headers['Access-Control-Expose-Headers'] = 'ETag'
-    		headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    		headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    		headers['Access-Control-Max-Age'] = '86400'
-  		end
 
 		end
 	end
